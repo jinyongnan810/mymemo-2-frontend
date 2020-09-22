d4c4a0005a0f608cf39b440adb107ff936c14428
Yuunan Kin <yuunan.kin@gmail.com>
2020-09-22 21:42:14 +0900

    change axios base url

--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const setAuthToken = (token) => {
+  axios.defaults.baseURL = process.env.API_ENDPOINT;
   if (token) {
     axios.defaults.headers["Authorization"] = `Bearer ${token}`;
   } else {
